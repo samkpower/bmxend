eabc68360044a53bdaf6394d178781bc59282446
IzzatBamieh <eib6@yahoo.com>
2016-10-08 15:28:18 -0400

    Add police report controller and model and paper trial

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :police_reports
   devise_for :users
   scope 'api/v1', module: 'v1', as: 'v1' do
     resources :users, :only => [:show, :create, :update]
     resources :bikes, :only => [:show, :create, :update]
+    resources :police_reports, :only => [:show, :create, :update]
   end
 end
